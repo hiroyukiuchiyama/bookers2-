6b78e8267fdd071c60dcdcccab97cdd430c4a531
hiroyukiuchiyama <uchiyamaworld@gmail.com>
2021-09-02 15:59:45 +0000

    bookers2 start

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+   get '/home/about' => 'homes#about'
+   devise_for :users
+   root to: 'homes#top'
+
+
+
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+end
