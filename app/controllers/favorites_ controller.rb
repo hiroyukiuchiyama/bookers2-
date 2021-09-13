e28119614aa2a79e669fa889a450cb41f645dc32
hiroyukiuchiyama <uchiyamaworld@gmail.com>
2021-09-13 07:22:52 +0000

    [finish2]応用課題6

--- app/controllers/favorites_ controller.rb
+++ app/controllers/favorites_ controller.rb
@@ -1,21 +1,15 @@
 class FavoritesController < ApplicationController
-   before_action :book_params
-
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
+    redirect_to book_path(book)
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-  end
-
-  private
-
-  def book_params
-    @book = Book.find(params[:book_id])
+    redirect_to book_path(book)
   end
 end
